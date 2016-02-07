395253013538a8e27e47aa79325866e619d33fb4
Maarten Busstra <busstra.m@gmail.com>
2016-02-07 04:04:53 +0100

    re-org

--- main.js
+++ main.js
@@ -1,58 +1,8 @@
-"use strict";
+import { Synth } from './lib/synth.js';
+import NOTES from './lib/notes.js';
 
-import Pico from 'node-pico';
-import { Oscillator } from './oscillator.js';
-import { square } from './waveforms.js';
-import NOTES from './notes.js';
+const synth = new Synth();
 
-function tone(oscs) {
-  return function fillBuffer(e) {
-    const out = e.buffers;
+synth.note = NOTES.A4;
 
-    for (let i = 0; i < e.bufferSize; i++) {
-      const samples = oscs.map(osc => osc.getNextSamples())
-      const [L, R] = mixer(samples);
-
-      out[0][i] = L;
-      out[1][i] = R;
-    }
-
-  };
-}
-
-function mixer(channels, {
-  master = 1,
-} = {}) {
-  let [L, R] = channels.reduce((x, y) => [x[0] + y[0], x[1] + y[1]]);
-  L = L / channels.length;
-  R = R / channels.length;
-
-  return [L * master, R * master];
-}
-
-
-Pico.play(tone([
-  new Oscillator({
-    freq: 880,
-    waveform: square,
-  }),
-  new Oscillator({
-    freq: 440,
-    waveform: square,
-  })
-]));
-
-
-// ,
-// {
-//   freq: 622.25,
-//   waveform: square,
-// },
-// {
-//   freq: 783.99,
-//   waveform: square,
-// },
-// {
-//   freq: 130.81,
-//   waveform: sine,
-// }
+synth.play();
