711998ccd787265a4fd5fd49cf233b6330ab2104
Maarten Busstra <busstra.m@gmail.com>
2016-02-07 05:53:48 +0100

    sequencer

--- main.js
+++ main.js
@@ -1,8 +1,22 @@
 import { Synth } from './lib/synth.js';
+import { Sequencer } from './lib/sequencer.js';
+
 import NOTES from './lib/notes.js';
 
 const synth = new Synth();
 
-synth.note = NOTES.A4;
+const seq = new Sequencer({
+  synth,
+  sequence: [
+    NOTES.C3, NOTES.DS3, NOTES.G3, NOTES.AS3,
+    NOTES.C3, NOTES.DS3, NOTES.G3, NOTES.AS3,
+    NOTES.C3, NOTES.DS3, NOTES.G3, NOTES.AS3,
+    NOTES.C3, NOTES.DS3, NOTES.G3, NOTES.AS3,
+    NOTES.C3, NOTES.DS3, NOTES.G3, NOTES.AS3,
+    NOTES.C3, NOTES.DS3, NOTES.G3, NOTES.AS3,
+    NOTES.C3, NOTES.DS3, NOTES.G3, NOTES.AS3,
+    NOTES.C3, NOTES.DS3, NOTES.G3, NOTES.AS3,
+  ],
+});
 
-synth.play();
+seq.start();
